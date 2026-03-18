6432e39fbcf776829b0e794aa7a3f42c8757da1e
jang121a-hue <jang121a@gmail.com>
2026-03-18 11:07:24 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'gomaworang-v1';
+const FILES = [
+  './gomaworang_inventory.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
